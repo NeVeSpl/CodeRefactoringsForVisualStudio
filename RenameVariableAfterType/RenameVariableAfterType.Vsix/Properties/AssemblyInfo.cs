a61365ffbd802c2ea525ecdf9220158339cb221c
NeVeSpl <nevespl@gmail.com>
2021-11-11 10:39:54 +0100

    RenameVariableAfterType add support for VS 2022 using only single vsix

--- RenameVariableAfterType/RenameVariableAfterType.Vsix/Properties/AssemblyInfo.cs
+++ RenameVariableAfterType/RenameVariableAfterType.Vsix/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RenameVariableAfterType.Vsix2022")]
+[assembly: AssemblyTitle("RenameVariableAfterType.Vsix")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RenameVariableAfterType.Vsix2022")]
+[assembly: AssemblyProduct("RenameVariableAfterType.Vsix")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
